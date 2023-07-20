43ac85db3f467c560040905061c30e7190b569d6
JheWeiJhang <c123456789q@gmail.com>
2023-07-20 19:15:14 +0800

    Add files via upload

--- js/d4_utils.js
+++ js/d4_utils.js
@@ -1,4 +1,10 @@
-function update_s0_data(number) {
+function hide_tables()
+{
+	$("#s0").hide();
+	$("#s1").hide();
+}
+
+function update_s0_data() {
 	$.getJSON('data/s0.json', function(data) {
 		$.each(data, function(i, item) {
 			var row='<tr>';
@@ -12,5 +18,22 @@ function update_s0_data(number) {
 			$('#s0 tbody').append(row);
 		});
 		$('#s0').DataTable();
+	});
+}
+
+function update_s1_data() {
+	$.getJSON('data/s1.json', function(data) {
+		$.each(data, function(i, item) {
+			var row='<tr>';
+			row+='<td style="display:none">'+data[i].index+'</td>';
+			row+='<td>'+data[i].name+'</td>';
+			row+='<td>'+data[i].experience_rank+'</td>';
+			row+='<td>'+data[i].glyph_rank+'</td>';
+			row+='<td>'+data[i].total_rank+'</td>';
+			row+='<td>'+data[i].group_rank+'</td>';
+			row+='</tr>';
+			$('#s1 tbody').append(row);
+		});
+		$('#s1').DataTable();
 	});
 }
