0f7f7939561318d62097fbfc5068758e45dcda10
JheWeiJhang <c123456789q@gmail.com>
2023-07-20 18:44:08 +0800

    Add files via upload

---
+++ js/d4_utils.js
@@ -0,0 +1,15 @@
+function update_s0_data(number) {
+	$.getJSON('data/s0.json', function(data) {
+		$.each(data, function(i, item) {
+			var row='<tr>';
+			row+='<td style="display:none">'+data[i].index+'</td>';
+			row+='<td>'+data[i].name+'</td>';
+			row+='<td>'+data[i].experience_rank+'</td>';
+			row+='<td>'+data[i].glyph_rank+'</td>';
+			row+='<td>'+data[i].total_rank+'</td>';
+			row+='<td>'+data[i].add_date+'</td>';
+			row+='</tr>';
+			$('#s0 tbody').append(row);
+		});
+	});
+}
